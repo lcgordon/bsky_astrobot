acf6c4249d8567f2bf8162174aeb56571e7a9434
lcgordon <contactlindseycg@gmail.com>
2024-04-25 17:31:30 +0200

    why is it not running the python

--- arxivapi.py
+++ arxivapi.py
@@ -4,7 +4,7 @@ import numpy as np
 import sys
 import feedparser #https://feedparser.readthedocs.io/en/latest/common-atom-elements.html
 
-# print("MADE IT TO PYTHON")
+print("MADE IT TO PYTHON")
 hrefurl = "http://rss.arxiv.org/atom/astro-ph"
 # hrefurl = "/Users/lindseygordon/Downloads/astro-ph"
 feed = feedparser.parse(hrefurl)
