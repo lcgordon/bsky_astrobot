684e7a9fb056b0ab541fd814b198871c9a7da17a
lcgordon <contactlindseycg@gmail.com>
2024-04-25 10:26:00 +0200

    Working bot

--- arxivapi.py
+++ arxivapi.py
@@ -2,27 +2,60 @@
 
 import numpy as np 
 import sys
-# import urllib.request as libreq
-# hrefurl = 'http://export.arxiv.org/api/query?search_query=all:electron&start=0&max_results=2'
-# with libreq.urlopen(hrefurl) as url:
-#     r = url.read()
-# # print(r)
+import feedparser #https://feedparser.readthedocs.io/en/latest/common-atom-elements.html
 
-import feedparser
 hrefurl = "http://rss.arxiv.org/atom/astro-ph"
+# hrefurl = "/Users/lindseygordon/Downloads/astro-ph"
 feed = feedparser.parse(hrefurl)
-
-# print(feed, "\n\n")
-# print(feed['feed'], "\n\n")
 nentries = len(feed['entries'])
-rando = np.random.randint(4)
-for i in range(1):
-    # print(feed['entries'][i]['link'])
-    # print(f"THIS IS ANOTHER TEST BUT BETTER???")
-    # print(feed['entries'][i]['title'])
-    # print(feed['entries'][i]['author'], "\n\n")
-    print(f"new on astro-ph: {feed['entries'][i]['title']} by {feed['entries'][i]['author']}; {feed['entries'][i]['link']}")
-
 # print(nentries)
-# print(dataToSendBack)
-sys.stdout.flush()
+if nentries == 0:
+    print("Arxiv RSS feed is down")
+    sys.stdout.flush()
+else: 
+    # pull random entry until you get a viable skeet
+    postable = False
+    while postable == False: 
+        i = np.random.randint(0, nentries)
+        # print(f"i = {i}")
+        link = feed['entries'][i]['link']
+        title = feed['entries'][i]['title']
+        # trim long author list: 
+        authorlist = feed['entries'][i]['author'].split(",")
+        lenauthorlist = len(authorlist)
+        # print(authorlist, lenauthorlist)
+        # fix author list
+        if lenauthorlist > 3: 
+            authors = f"{authorlist[0]}+"
+        else:
+            authors = feed['entries'][i]['author']
+        # generate initial skeet
+        miniskeet = f"new on astro-ph:  by {authors}; {link}"
+        if len(miniskeet) > 300: #if somehow still too big, continue on with a different paper
+            continue
+        
+        skeet = f"new on astro-ph: {title} by {authors}; {link}"
+        # if the skeet is too long, cut it down: 
+        if len(skeet) >= 300: 
+            # if title too long, cut it down
+            miniskeet = f"new on astro-ph: {title} by {authors}; {feed['entries'][i]['link']}"
+            counter = len(skeet) - len(miniskeet) # how many characters are left
+            titlesplit = title.split(" ")
+            print(titlesplit)
+            title = ""
+            # get the chunk that will fit: the first n words + n spaces that are smaller than counter
+            titlelen = 0 
+            h = 0
+            while titlelen < (counter - 5): 
+                titlelen += 1 + len(titlesplit[h])
+                title += " " + titlesplit[h]
+                h += 1
+
+            title += "..."
+        skeet = f"new on astro-ph: {title} by {authors}; {link}"
+        postable = True
+
+
+    # print(f"skeet length: {len(skeet)}")
+    print(skeet)
+    sys.stdout.flush()
--- src/lib/getPostText.ts
+++ src/lib/getPostText.ts
@@ -1,13 +1,9 @@
-import { createRequire } from 'module'
+import { createRequire } from 'module' //https://stackoverflow.com/questions/55997921/how-do-i-change-these-require-statements-for-these-modules-to-use-import-stateme
 
 export default async function getPostText() {
   const require = createRequire(import.meta.url)
   // Generate the text for your post here. You can return a string or a promise that resolves to a string
-  
-  // start my code
-  // const child_process = require("child_process");//.spawnSync;
-  // const pythonProcess = child_process.spawnSync('python',["./arxivapi.py"]);
-
+  //https://stackoverflow.com/questions/23450534/how-to-call-a-python-function-from-node-js
   //https://stackoverflow.com/questions/14332721/node-js-spawn-child-process-and-get-terminal-output-live
   var child_process = require('child_process');
 
@@ -16,33 +12,6 @@ export default async function getPostText() {
   if(child.error) {
       console.log("ERROR: ",child.error);
   }
-  // console.log("stdout: ",child.stdout);
-  // console.log("stderr: ",child.stderr);
-  // console.log("exist code: ",child.status);
-  return child.stdout;
-
-
-  // pythonProcess.stdout.on('data', (data:any) => {
-  //   // Do something with the data returned from python script
-  //   console.log(`${data.toString()}`);
-  //  });
-
-  //  var scriptOutput = "";
-
-  //   // pythonProcess.stdout.setEncoding('utf8');
-  //   pythonProcess.stdout.on('data', function(data:any) {
-  //       console.log('stdout: ' + data);
-
-  //       data=data.toString();
-  //       scriptOutput+=data;
-  //   });
-  //   console.log(`${scriptOutput}`);
-
-    // pythonProcess.on('close', function(code) {
-    //   callback(scriptOutput,code);
-    //  });
-
-
-  // return "THIS IS A BOT TEST POST!";
+  return child.stdout; 
 
 }
